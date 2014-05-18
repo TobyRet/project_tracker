885a3ef2f8f7cab57937a14bf3f201929b94ed4a
Toby Retallick <toby.retallick@gmail.com>
2014-05-18 18:45:05 +0100

    Users redirected to tprojects page if they try visiting project that does not exist

--- project_tracker/app/controllers/projects_controller.rb
+++ project_tracker/app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all
 	end
@@ -9,8 +11,6 @@ class ProjectsController < ApplicationController
 	end
 
 	def create
-		@project = Project.new(project_params)
-
 		if @project.save
 			flash[:notice] = "Project has been created."
 			redirect_to @project
@@ -22,15 +22,12 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
 	end
 
 	def edit 
-		@project = Project.find(params[:id])
 	end
 
 	def update 
-		@project = Project.find(params[:id])
 		if @project.update(project_params)
 			flash[:notice] = "Project has been updated."
 			redirect_to @project
@@ -41,7 +38,6 @@ class ProjectsController < ApplicationController
 	end
 
 	def destroy
-		@project = Project.find(params[:id])
 		@project.destroy
 
 		flash[:notice] = "Project has been destroyed."
@@ -54,6 +50,13 @@ class ProjectsController < ApplicationController
 
 		def project_params
 			params.require(:project).permit(:name, :description)
+		end
+
+		def set_project 
+			@project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The project you were looking" + " for could not be found."
+			redirect_to projects_path
 		end
 
 end
